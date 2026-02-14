f6e4b57f157e2d8ee28f43fa562d3ef33200fd90
Fabian Santer <fsanter@student.tugraz.at>
2026-02-14 17:32:44 +0100

    Debug the api key!

--- src/app/lib/supabase.ts
+++ src/app/lib/supabase.ts
@@ -3,4 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
+console.log('url, key', supabaseUrl, supabaseAnonKey); // DELETE!!!!!
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);

9f813ebf36c48864ba4fa9e0bbfdc33088a167cb
Fabian Santer <fsanter@student.tugraz.at>
2026-02-14 17:49:25 +0100

    Removed debug statement

--- src/app/lib/supabase.ts
+++ src/app/lib/supabase.ts
@@ -3,6 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-console.log('url, key', supabaseUrl, supabaseAnonKey); // DELETE!!!!!
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
